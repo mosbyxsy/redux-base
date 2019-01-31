c79ca328d0557ba14ce5f8915009deb5d419d646
Mosby192 <545610830@qq.com>
2019-01-31 23:37:40 +0800

    redux-actions action

--- src/action/todoListAction.js
+++ src/action/todoListAction.js
@@ -1,13 +1,16 @@
 import todoListConst from '../constant/todoListConst';
+import {createAction, createActions} from 'redux-actions';
 
-function addItem(item) {
+const addItem = createAction(todoListConst.ADD_ITEM, (item) => ({todoList: item}));
+
+/*function addItem(item) {
     return {
         type: todoListConst.ADD_ITEM,
         payload: {
             todoList: item
         }
     }
-}
+}*/
 
 function delItem(id) {
     return {
@@ -18,7 +21,13 @@ function delItem(id) {
     }
 }
 
+/*
 export default {
     addItem,
     delItem
-}
+}*/
+
+export default createActions({
+    [todoListConst.ADD_ITEM]: (item) => ({todoList: item}) ,
+    [todoListConst.DEl_ITEM]: (id) => ({id})
+});
