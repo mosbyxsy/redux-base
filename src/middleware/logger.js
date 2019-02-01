04646881907b6e1fd58dd21c9ba495d96e25c8a7
Mosby192 <545610830@qq.com>
2019-02-01 23:49:23 +0800

    updata

--- src/middleware/logger.js
+++ src/middleware/logger.js
@@ -1,5 +1,6 @@
 
 // const doNothingMidddleware = ({dispatch, getState}) => next => action => next(action)
+// const doNothingMidddleware = ({dispatch, getState}) => next => f(修改后的dispatch)
 // 中间件其实就是修改dispatch
 export default ({dispatch, getState}) => next => action => {
     console.log(getState(), action);
